4be201ebcf7638b27304ddea89e29df3643c9b87
Viraj Kothari <virajrkothari@gmail.com>
2019-12-13 01:59:29 -0800

    fragment created

---
+++ CampusNavigation/app/src/main/java/com/example/campusnavigation/ET.kt
@@ -0,0 +1,24 @@
+package com.example.campusnavigation
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ET : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_et, container, false)
+    }
+
+
+}
--- CampusNavigation/app/src/main/java/com/example/campusnavigation/MapsActivity.kt
+++ CampusNavigation/app/src/main/java/com/example/campusnavigation/MapsActivity.kt
@@ -1,7 +1,12 @@
 package com.example.campusnavigation
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,17 +15,62 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+import androidx.navigation.ui.NavigationUI
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.navigation.NavigationView
+
+//import com.example.campusnavigation.databinding.ActivityMainBinding
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback , NavigationView.OnNavigationItemSelectedListener{
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     private lateinit var mMap: GoogleMap
+
+    private lateinit var drawer: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.map)
+
+        toggle = ActionBarDrawerToggle(this, drawer, toolbar, 0, 0)
+        drawer.addDrawerListener(toggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+
+//        toolbar = findViewById(R.id.toolbar)
+//        setSupportActionBar(toolbar)
+//
+//        drawerLayout = findViewById(R.id.drawer_layout)
+//        navView = findViewById(R.id.nav_view)
+//
+//        val toggle = ActionBarDrawerToggle(
+//            this, drawerLayout, toolbar, 0, 0
+//        )
+//        drawerLayout.addDrawerListener(toggle)
+//        toggle.syncState()
+//        navView.setNavigationItemSelectedListener(this)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+//        val binding = DataBindingUtil.setContentView<ActivityMapsBinding>(this, R.layout.activity_maps)
+//        dL = binding.drawerLayout
+
+
     }
 
     /**
@@ -40,4 +90,23 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toggle.syncState()
+    }
+
+//    override fun onConfigurationChanged(newConfig: Configuration?) {
+//        super.onConfigurationChanged(newConfig)
+//        toggle.onConfigurationChanged(newConfig)
+//    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
