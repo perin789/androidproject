97a9b4ab89928314baea794db338823308f81d68
Viraj Kothari <virajrkothari@gmail.com>
2019-12-13 21:18:51 -0800

    added navigation and drawer

---
+++ CampusNavigation/app/src/main/java/com/android/example/campusnavigation/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.android.example.campusnavigation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        lateinit var Login: Button
+
+
+//        Login = findViewById(R.id.button)!!
+//
+//        Login.setOnClickListener {
+//            val intent = Intent(this, MapsActivity::class.java)
+//            startActivity(intent);
+//        }
+
+
+
+    }
+}
